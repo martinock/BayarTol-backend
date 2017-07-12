3a27a647f20f3924ee4b92bb692d1b16565d2f2e
Malvin Juanda <13514044@std.stei.itb.ac.id>
2017-07-12 23:02:56 +0700

    add transaction and register toll

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,7 @@
 
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const dateTime = require('node-datetime');
 admin.initializeApp(functions.config().firebase);
 
 exports.register = functions.https.onRequest((req, res) => {
@@ -17,22 +18,44 @@ exports.register = functions.https.onRequest((req, res) => {
 						disabled: false
 					})
 					.then(function(userRecord) {
-						//Set New User Data
-						var data = {
-							name: req.body.name,
-							phone_number: req.body.phone_number,
-							address: req.body.address,
-							money: 0
+						if (req.body.role == 'user') {
+							var data = {
+								name: req.body.name,
+								phone_number: req.body.phone_number,
+								address: req.body.address,
+								money: 0
+							};
+
+							admin.database().ref("/users").child(userRecord.uid).push().set(data);
+
+							var body = {
+								uid: userRecord.uid
+							};
+
+							//Set New User Data
+							res.type('application/json');
+							res.status(200).send(JSON.stringify(body));
+
 						}
+						else if (req.body.role == 'toll') {
+							var data = {
+								name: req.body.name,
+								cost: req.body.cost
+							};
 
-						admin.database().ref("/users").child(userRecord.uid).push().set(data)
+							admin.database().ref("/tolls").child(userRecord.uid).push().set(data);
 
-						var body = {
-							uid: userRecord.uid
-						};
+							var body = {
+								uid: userRecord.uid
+							};
 
-						res.type('application/json');
-						res.status(200).send(JSON.stringify(body));
+							//Set New User Data
+							res.type('application/json');
+							res.status(200).send(JSON.stringify(body));
+						}
+						else {
+
+						}
 					})
 					.catch(function(error) {
 						res.status(500).send({ error: error.message });
@@ -54,6 +77,36 @@ exports.profile = functions.https.onRequest((req, res) => {
 			admin.database().ref("/users").child(req.query.uid).on("value", function(snapshot) {
 				res.type('application/json');
 				res.status(200).send(snapshot.val());
+			}, function (errorObject) {
+				res.status(errorObject.code).send({ error: 'Read data failed' });
+			});
+	      	break;
+	    default:
+			res.status(400).send({ error: 'Undefined Request Method' });
+			break;
+  	}
+});
+
+exports.transaction = functions.https.onRequest((req, res) => {
+	switch (req.method) {
+	    case 'GET':
+			var dt = dateTime.create();
+			var formattedDatetime = dt.format('Y-m-d H:M:S');
+			var tollData;
+
+			admin.database().ref("/tolls").child(req.query.tid).on("value", function(snapshot) {
+				tollData = snapshot.val();
+
+				var data = {
+					uid: req.query.uid,
+					tid: req.query.tid,
+					cost: tollData.cost,
+					datetime: formattedDatetime
+				}
+
+				admin.database().ref("/transactions").child(req.body.uid).push().set(data)
+
+				
 			}, function (errorObject) {
 				res.status(errorObject.code).send({ error: 'Read data failed' });
 			});
